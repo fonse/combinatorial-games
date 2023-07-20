d13c165aea1bb429126caec524bab499a680849e
Alejandro Alfonso <alejandro@tiendanube.com>
2023-07-20 11:21:17 -0300

    Rename Take and Break games

--- TakeAndBreak.hs
+++ TakeAndBreak.hs
@@ -3,7 +3,7 @@ import Util ( choose )
 import Data.Bits ( Bits(testBit) )
 import Data.List ( nub, sort )
 
-data TakeBreak = TakeBreak { rules :: [Int], heaps :: [Int] } deriving Show
+data TakeAndBreak = TakeAndBreak { rules :: [Int], heaps :: [Int] } deriving Show
 
 -- Can you remove n beans while leaving behind m heaps according to the rules?
 validMove :: [Int] -> Int -> Int -> Bool
@@ -23,12 +23,12 @@ split heapSize n = nub . (sort <$>) $ do
   (h, hs) <- choose heaps
   (hs++) <$> split h 1
 
-instance Combinatorial TakeBreak where
+instance Combinatorial TakeAndBreak where
   leftMoves = rightMoves
-  rightMoves (TakeBreak rules heaps) =
+  rightMoves (TakeAndBreak rules heaps) =
     do
       n <- [0..(length rules-1)]
       (h,hs) <- choose heaps
       let afterTake = h-n
       newHeaps <- split afterTake (afterTake-1)
-      if validMove rules n (length newHeaps) then return (TakeBreak rules (newHeaps ++ hs)) else []
+      if validMove rules n (length newHeaps) then return (TakeAndBreak rules (newHeaps ++ hs)) else []
